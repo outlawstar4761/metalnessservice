c757255059c21c953dd9928e792f1f9471ca3e2a
John J. Watson <outlawstar4761@gmail.com>
2021-06-29 19:03:12 +0100

    fixing http server

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from MetalnessCalculator import MetalnessCalculator
+from MetalnessCalculator.MetalnessCalculator import MetalnessCalculator
+import pandas as pd
 import time
 
 hostName = "localhost"
 serverPort = 8080
 
 class Server(BaseHTTPRequestHandler):
+    metalSet = pd.read_csv('./data/dark_lyrics.csv.4', sep=',', escapechar='\\')
+    noMetalSet = pd.read_csv('./data/light_lyrics.csv.1', encoding='utf-8', sep=',', dtype=str,escapechar='\\')
+    metalCalculator = MetalnessCalculator(metalSet,noMetalSet)
     def __init__(self, request, client_address, server):
-        print("constructing server..")
-        self.metalCalculator = MetalnessCalculator()
         BaseHTTPRequestHandler.__init__(self, request, client_address, server)
     def do_GET(self):
+        print(self.metalCalculator.words_metalness_df.head())
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>How Metal is this?</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        # self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>POST strings to this address to find out how metal they are!</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
     def do_POST(self):
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len).decode('utf-8')
-        print(post_body)
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        response = str(self.metalCalculator.calculate_words_metalness(post_body)).encode()
+        response = str(self.metalCalculator.calculate_metalness_score(post_body)).encode()
         self.wfile.write(response)
 
 if __name__ == "__main__":
